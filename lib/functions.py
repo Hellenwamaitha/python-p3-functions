acda088fe4929e145be8a388eba969c062cd497a
hellenwamaitha <hellenwama254@gmail.com>
2023-08-18 14:53:58 +0300

    passed lab

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print("Hello, " + name + "!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    return number / 2
+
+# Call functions here
+greet("Hellen")
+greet_programmer()
+greet_with_default()
+result = add(5, 7)
+print(result)
+halved = halve(10)
+print(halved)
+
+    
